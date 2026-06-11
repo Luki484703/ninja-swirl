0fdd6eb8d8820163222bc119cb038b6ddc69be74
Luki484703 <LukasAust@gmx.net>
2026-06-11 13:52:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE='ninja-swirl-v1';
+const CACHE='ninja-swirl-v2';
 const FILES=['/ninja-swirl/','/ninja-swirl/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(clients.claim());
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()));
 });
 self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+  e.respondWith(fetch(e.request).then(res=>{
     return caches.open(CACHE).then(c=>{c.put(e.request,res.clone());return res;});
-  }).catch(()=>caches.match('/ninja-swirl/index.html'))));
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('/ninja-swirl/index.html'))));
 });
