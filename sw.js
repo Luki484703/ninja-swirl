6730e51b850fc7fbecac974ca396f11168eb25d4
Luki484703 <LukasAust@gmx.net>
2026-06-12 07:57:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ninja-swirl-v5';
+const CACHE='ninja-swirl-v6';
 const FILES=['/ninja-swirl/','/ninja-swirl/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
